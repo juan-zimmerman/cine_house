ecd166e49762eb6ecc284d06323afc88c78c955c
Juan Zimmerman de Souza Oliveira <juanito.tevez@gmail.com>
2024-04-03 23:32:43 -0300

    barra de pesquisa

--- paginas/app.js
+++ paginas/app.js
@@ -2,3 +2,26 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('searchInput');
+    const imgDiv = document.getElementById('img_div');
+    const imgs = imgDiv.querySelectorAll('.img');
+
+    searchInput.addEventListener('input', function(event) {
+        const searchTerm = event.target.value.toLowerCase();
+
+        imgs.forEach(function(img) {
+            const altText = img.getAttribute('alt').toLowerCase();
+            const imgLink = img.parentElement.getAttribute('href');
+
+            if (altText.includes(searchTerm)) {
+                img.style.display = 'inline-block';
+                img.parentElement.style.display = 'inline-block';
+            } else {
+                img.style.display = 'none';
+                img.parentElement.style.display = 'none';
+            }
+        });
+    });
+});
